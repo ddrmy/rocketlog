95a86d6a5b3ff62c089b19c1ade06cb7b6105968
Gabriel Sarti <gabriel_rti@hotmail.com>
2026-06-17 16:18:45 -0300

    add: controlelr to create delivery

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,30 @@
+import { prisma } from "@/database/prisma"
 import { Request, Response } from "express"
+import z from "zod"
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    response.json({ message: "ok" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    })
+
+    const { user_id, description } = bodySchema.parse(request.body)
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    })
+
+    response.status(201).json()
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany()
+
+    return response.json(deliveries)
   }
 }
 
