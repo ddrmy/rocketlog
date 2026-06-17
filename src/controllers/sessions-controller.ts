740bad56cf3bb9db7490f50ecd22aac281f318d9
Gabriel Sarti <gabriel_rti@hotmail.com>
2026-06-17 15:05:10 -0300

    add: config and validation token

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,48 @@
+import { prisma } from "@/database/prisma"
+import { authConfig } from "@/configs/auth"
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
+import z from "zod"
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign(
+      {
+        role: user.role ?? "customer",
+      },
+      secret,
+      {
+        subject: user.id,
+        expiresIn,
+      },
+    )
+
+    return response.json({ token })
   }
 }
 
